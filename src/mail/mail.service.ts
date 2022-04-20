90bfb2296bd4136fbea3a3bbf29ea40052632214
AlekseiPetrovDev <iamalexey.dev@gmail.com>
2022-04-20 23:18:07 +0300

    checK

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import sgMail from "@sendgrid/mail"
+
+@Injectable()
+export class MailService {
+  constructor() {
+    const key = String(process.env.SENDGRID_KEY);
+    sgMail.setApiKey(key);
+  }
+
+  async sendVerificationCode(email: string, code: string ) {
+    const mail = {
+      to: email,
+      subject: "Verfification code",
+      from: 'hello@dialect.to',
+      html: `<h1>${code}</h1`
+    }
+    try {
+      const transport = await sgMail.send(mail);
+      // avoid this on production. use log instead :)
+      console.log(`E-Mail sent to ${transport}`);
+    } catch(e: any) {
+      console.log(e["response"].body);
+    }
+  }
+}
