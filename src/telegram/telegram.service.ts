374888eafe6d22a7ba1492391c6d3a6c15ed9edc
AlekseiPetrovDev <iamalexey.dev@gmail.com>
2022-04-28 22:33:34 +0300

    added to value chat id for monitor

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,3 +1,4 @@
+import { Wallet } from '@project-serum/anchor';
 import {
     Update,
     Ctx,
@@ -16,7 +17,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
         const addresses = await this.prisma.address.findMany({
             orderBy: [{updatedAt: "desc"}],
             where: {
-                value: ctx.update.message.from.usernam,
+                value: {
+                    contains: ctx.update.message.from.username,
+                },
+                type: "telegram",
                 verified: false 
             }
         });
@@ -25,6 +29,15 @@ import { PrismaService } from 'src/prisma/prisma.service';
             await ctx.reply("You're already receiving notifications or your wallet not attached")
         } else {
             await ctx.reply(`Welcome to dialect, ${ctx.update.message.from.first_name}.\nHere is your verification code: ${addresses[0].verificationCode}`);
+            // TODO:: Not best solution to keep <username>;<id>, need to solve it from DB structure 
+            const value = `${ctx.update.message.from.username};${ctx.update.message.from.id}`
+            await this.prisma.address.updateMany({
+                where: {
+                  id: addresses[0].id,
+                  walletId: addresses[0].walletId,
+                },
+                data: { value }
+            });
         }
     }
   }
