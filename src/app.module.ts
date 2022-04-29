cafe26a8bf7b5ffaebb406275065e0fa288e37d4
G <g@Gs-MacBook-Pro.local>
2022-04-28 16:31:43 -1000

    Web2SubscriberModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DappModule } from './dapp/dapp.module';
 import { MailModule } from './mail/mail.module';
 import { SmsVerificationModule } from './sms/sms.module';
 import { TelegramModule } from './telegram/telegram.module';
+import { Web2SubscriberModule } from './web2Subscriber/web2Subscriber.module';
 
 @Module({
-  imports: [PrismaModule, WalletModule, DappModule, MailModule, SmsVerificationModule, TelegramModule],
+  imports: [PrismaModule, WalletModule, DappModule, MailModule, SmsVerificationModule, TelegramModule, Web2SubscriberModule],
   providers: [],
 })
 export class AppModule {}
