421dd9159acce3c21e42a2875f58187695b2f5eb
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-05-26 19:41:25 +0400

    Fix type definition in DTO that broke swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,32 +7,10 @@ import { SmsVerificationModule } from './sms/sms.module';
 import { TelegramModule } from './telegram/telegram.module';
 import { ConfigModule } from '@nestjs/config';
 import { DialectModule } from './dialect/dialect.module';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        autoLogging: {
-          ignore: (msg) => {
-            if (msg.statusCode && msg.statusCode >= 400) {
-              return false;
-            }
-            return process.env.ENVIRONMENT === 'production';
-          },
-        },
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            colorize: process.env.ENVIRONMENT === 'local-development',
-            translateTime: true,
-            singleLine: true,
-            ignore: 'pid,hostname',
-          },
-        },
-      },
-    }),
     PrismaModule,
     WalletModule,
     DappModule,
