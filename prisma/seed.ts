36ecf7277a86562f0daba8ef686d48a979a36787
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-07-17 19:59:31 +0400

    Add notification types to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,7 +25,7 @@ async function seedDev() {
     },
     update: {},
   });
-  await prisma.dapp.upsert({
+  const dialect = await prisma.dapp.upsert({
     where: {
       id: '44a34c33-9938-49f3-935c-dcbf35d1b5a3',
     },
@@ -33,6 +33,44 @@ async function seedDev() {
       id: '44a34c33-9938-49f3-935c-dcbf35d1b5a3',
       name: 'dialect',
       publicKey: 'D1ALECTfeCZt9bAbPWtJk7ntv24vDYGPmyS7swp7DY5h',
+    },
+    update: {},
+  });
+
+  await prisma.notificationType.upsert({
+    where: {
+      dappId_id: {
+        dappId: dialect.id,
+        id: 'd388f444-fa34-4e4d-a306-a1958a50640d',
+      },
+    },
+    create: {
+      id: 'd388f444-fa34-4e4d-a306-a1958a50640d',
+      dappId: dialect.id,
+      name: 'Dapp notification type 1',
+      trigger: 'Dapp notification type trigger 1',
+      humanReadableId: 'notification_1',
+      enabled: true,
+      tags: [],
+    },
+    update: {},
+  });
+
+  await prisma.notificationType.upsert({
+    where: {
+      dappId_id: {
+        dappId: dialect.id,
+        id: 'b63184eb-e25f-408f-aca3-d5961dbc2edc',
+      },
+    },
+    create: {
+      id: 'b63184eb-e25f-408f-aca3-d5961dbc2edc',
+      dappId: dialect.id,
+      name: 'Dapp notification type 2',
+      trigger: 'Dapp notification type trigger 2',
+      humanReadableId: 'notification_2',
+      enabled: false,
+      tags: [],
     },
     update: {},
   });
